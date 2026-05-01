c7497cabe5ae4d319a52205f1a09e9654521f4ab
ba to goor <daschera1492@outlook.com>
2026-05-01 14:55:13 +0330

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // اگر قصد دارید از تصاویر خارجی استفاده کنید، تنظیمات images را اینجا اضافه کنید
+  images: {
+    domains: [],
+  },
+}
+
+module.exports = nextConfig
